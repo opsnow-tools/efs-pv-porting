eb028ae25f4c15bf19643f04ae01a2cb8f00b709
longlg88 <longlg88@naver.com>
2019-09-06 11:48:42 +0900

    Test

---
+++ run.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import getopt
+
+def help():
+    print("print help usage")
+    return
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"eih", ["help"])
+    except getopt.GetoptError as err:
+        print(str(err))
+        help()
+        sys.exit(1)
+
+
+    for opt,arg in opts:
+        if ( opt == "-e" ) or ( opt == "--export" ):
+            print("export directory = "+arg)
+        elif ( opt == "-i" ) or ( opt == "--import" ):
+            print("Importing PV...")
+        elif ( opt == "-h" ) or ( opt == "--help" ):
+            help()
+
+    return
+
+if __name__ == "__main__":
+    main()
