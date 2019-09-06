d71c135a74189d9d6c1966425890e6a1a94a074f
longlg88 <longlg88@naver.com>
2019-09-06 18:32:08 +0900

    Test

--- run.py
+++ run.py
@@ -8,12 +8,53 @@ def help():
     print("print help usage")
     return
 
+def export_pv_yaml(args):
+    for arg in {k : v for k,v in args.iteritems() if 'pv-name-' in k}.values():
+
+
+        #exportFileCmd = "kubectl get pv "+arg_pv+" -oyaml > "+arg_file+".yaml"
+        #p2 = Popen(exporFileCmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        #cmd_exportFile=p2.stdout.read()
+
 def export(args):
+    ## check directory & make directory
+    if os.path.exists(args):
+        print("Temp directory already exists")
+    else:
+        os.mkdir(args)
+        print("Temp directory create")
+
     ## export PVs to yaml files
+
     exportCmd="kubectl get pv"
     p1 = Popen(exportCmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-    cmd_export = p1.stdout.read()
-    print(cmd_export.decode('utf-8'.split()))
+
+    parsing_pv, parsing_file = [], []
+    parsing_export, parsing_export_pv, parsing_export_file = dict(), dict(), dict()
+    cnt = 0
+    while True:
+        cmd_export = p1.stdout.readline()
+        if not cmd_export:
+            break
+        cmd_export = cmd_export.encode('ascii')
+
+        if 'Available' in cmd_export or 'NAME' in cmd_export:
+            continue
+        else:
+            prs_ex = cmd_export.split()
+            key_pv_name, value_pv_name = 'pv-name-'+str(cnt), prs_ex[0]
+            key_file_name, value_file_name = 'file-name-'+str(cnt), prs_ex[5].split('/')[1]
+            parsing_export_pv[key_pv_name] = value_pv_name
+            parsing_export_file[key_file_name] = value_file_name
+            parsing_export.update(parsing_export_pv)
+            parsing_export.update(parsing_export_file)
+            cnt=cnt+1
+
+
+    ################################################################
+    export_pv_yaml(parsing_export)
+
+#def under_construction_file(args):
 
 def main():
     try:
