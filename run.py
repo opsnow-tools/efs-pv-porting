1f3e838bfb8a8028def5d3f52a0e5cf2364e05b9
longlg88 <longlg88@naver.com>
2019-09-06 13:33:48 +0900

    Test

--- run.py
+++ run.py
@@ -1,10 +1,19 @@
 import os
 import sys
 import getopt
+from subprocess import Popen, PIPE, STDOUT
+import subprocess
 
 def help():
     print("print help usage")
     return
+
+def export(args):
+    ## export PVs to yaml files
+    exportCmd="kubectl get pv"
+    p1 = Popen(exportCmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    cmd_export = p1.stdout.read()
+    print(cmd_export.decode('utf-8'.split()))
 
 def main():
     try:
@@ -18,6 +27,7 @@ def main():
     for opt,args in opts:
         if ( opt == "-e" ) or ( opt == "--export" ):
             print("export directory = "+args)
+            export(args)
         elif ( opt == "-i" ) or ( opt == "--import" ):
             print("Importing PV... "+args)
         elif ( opt == "-h" ) or ( opt == "--help" ):
