4854d38ad590ceafb0dd12964bd4870ae7dfeaf7
longlg88 <longlg88@naver.com>
2019-09-11 11:21:10 +0900

    Add kube config copy

--- run.py
+++ run.py
@@ -178,7 +178,7 @@ def init_context(args):
     with open(new_config_file, 'w') as yaml_file:
         yaml.dump(new_config, yaml_file, default_flow_style=False)
 
-    now = datetime.datetime.utcnow()
+    now = datetime.datetime.utcnow().strftime('%s')
     shutil.copy2(home+'/.kube/config', home+'/.kube/config-backup-efspvporting-'+str(now))
     shutil.copy2(new_config_file, home+'/.kube/config')
 
